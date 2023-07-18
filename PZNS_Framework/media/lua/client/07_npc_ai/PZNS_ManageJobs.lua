14c20e129ec4c9e653101fd3839a091bf5a2e0b7
shadowhunter100 <131904770+shadowhunter100@users.noreply.github.com>
2023-07-17 17:06:23 -0700

    Added "Remove From Group" to Jobs context menu.

--- PZNS_Framework/media/lua/client/07_npc_ai/PZNS_ManageJobs.lua
+++ PZNS_Framework/media/lua/client/07_npc_ai/PZNS_ManageJobs.lua
@@ -1,4 +1,6 @@
 local PZNS_UtilsDataNPCs = require("02_mod_utils/PZNS_UtilsDataNPCs");
+local PZNS_UtilsNPCs = require("02_mod_utils/PZNS_UtilsNPCs");
+local PZNS_NPCGroupsManager = require("04_data_management/PZNS_NPCGroupsManager");
 --[[
     Cows: The differences between an action, an order, and a job is that an "action" can be queued, rearranged, or cleared as needed.
     An order may be made up of multiple actions and possibly carried out differently depending on job.
@@ -8,28 +10,38 @@ PZNS_CellZombiesList = {}; -- Cows: Init PZNS_CellZombiesList as an empty table,
 
 local function doNothing()
 end
-
+-- WIP - Cows: Need to rethink how Global variables are used...
 PZNS_JobsText = {
     Companion = "Companion",
     -- Farmer = "Farmer",     -- WIP - Cows: Commented out until implementation is ready.
     -- Engineer = "Engineer", -- WIP - Cows: Commented out until implementation is ready.
     Guard = "Guard",
-    Undertaker = "Undertaker"
+    Undertaker = "Undertaker",
+    Remove = "Remove From Group"
 };
---
+-- WIP - Cows: Need to rethink how Global variables are used...
 PZNS_Jobs = {
     Companion = PZNS_JobCompanion,
-    Farmer = doNothing,
-    Engineer = doNothing,
+    -- Farmer = doNothing, -- WIP - Cows: Commented out until implementation is ready.
+    -- Engineer = doNothing, -- WIP - Cows: Commented out until implementation is ready.
     Guard = PZNS_JobGuard,
-    Undertaker = PZNS_JobUndertaker
+    Undertaker = PZNS_JobUndertaker,
+    Remove = doNothing
 };
-
 --- Cows: Helper function for PZNS_UpdateAllJobsRoutines(), can also be used to update an npc's routine when their job is changed.
 ---@param npcSurvivor any
 function PZNS_UpdateNPCJobRoutine(npcSurvivor)
     if (npcSurvivor == nil) then
         return;
+    end
+    -- Cows: Check if the NPC's job is currently "Remove" or "Remove Grom Group".
+    if (npcSurvivor.jobName == "Remove" or npcSurvivor.jobName == "Remove From Group") then
+        -- WIP - Cows: Being removed from the group which should make the NPC angry/very angry.
+        PZNS_NPCSpeak(npcSurvivor, "Why me?", "Negative");
+        PZNS_NPCGroupsManager.removeNPCFromGroupBySurvivorID(npcSurvivor.groupID, npcSurvivor.survivorID);
+        PZNS_UtilsNPCs.PZNS_SetNPCGroupID(npcSurvivor, nil);
+        PZNS_UtilsNPCs.PZNS_SetNPCJob(npcSurvivor, "Wander"); -- WIP - Cows: Currently there is no NPC "wandering" AI nor job... so this is a placeholder
+        return;                                               -- Cows: Stop Processing, the npc is no longer in the group.
     end
     -- Cows: Only update living npcSurvivor routine.
     if (npcSurvivor.isAlive == true) then
