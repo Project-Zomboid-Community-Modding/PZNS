a14a0c2b093fde5d15ee8f8803a9fc04f48d7d9f
shadowhunter100 <131904770+shadowhunter100@users.noreply.github.com>
2023-07-25 16:12:41 -0700

    Placeholder check in.

---
+++ PZNS_Framework/media/lua/client/07_npc_ai/PZNS_JobWanderInBuilding.lua
@@ -0,0 +1,14 @@
+local PZNS_UtilsNPCs = require("02_mod_utils/PZNS_UtilsNPCs");
+local PZNS_GeneralAI = require("07_npc_ai/PZNS_GeneralAI");
+
+---comment
+---@param npcSurvivor any
+function PZNS_JobWanderInBuilding(npcSurvivor)
+    if (PZNS_UtilsNPCs.IsNPCSurvivorIsoPlayerValid(npcSurvivor) == false) then
+        return;
+    end
+    if (PZNS_GeneralAI.PZNS_IsNPCBusyCombat(npcSurvivor) == true) then
+        return; -- Cows Stop Processing and let the NPC finish its actions.
+    end
+    --- Cows: Now we can assume the NPC is valid and not busy in combat below this line ---
+end
