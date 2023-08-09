f0623ccdd4ffc1e84d7c97c9ffe7f5706f515dbb
shadowhunter100 <131904770+shadowhunter100@users.noreply.github.com>
2023-08-09 15:02:01 -0700

    Added additional checks to context menu invite.

--- PZNS_Framework/media/lua/client/08_mod_contextmenu/PZNS_ContextMenuInvite.lua
+++ PZNS_Framework/media/lua/client/08_mod_contextmenu/PZNS_ContextMenuInvite.lua
@@ -1,3 +1,4 @@
+local PZNS_UtilsDataNPCs = require("02_mod_utils/PZNS_UtilsDataNPCs");
 local PZNS_UtilsNPCs = require("02_mod_utils/PZNS_UtilsNPCs");
 local PZNS_PlayerUtils = require("02_mod_utils/PZNS_PlayerUtils");
 local PZNS_NPCGroupsManager = require("04_data_management/PZNS_NPCGroupsManager");
@@ -8,7 +9,7 @@ local PZNS_NPCsManager = require("04_data_management/PZNS_NPCsManager");
 ---@param context any
 ---@param worldobjects any
 function PZNS_ContextMenuInvite(mpPlayerID, context, worldobjects)
-    local isInviteeFound = false;
+    local invitableCount = 0;
     local playerSurvivor = getSpecificPlayer(mpPlayerID);
     local playerGroupID = "Player" .. tostring(mpPlayerID) .. "Group";
     --
@@ -20,6 +21,7 @@ function PZNS_ContextMenuInvite(mpPlayerID, context, worldobjects)
     --
     for i = 0, objectsListSize do
         local currentObj = squareObjects:get(i);
+        local canInvite = false;
         --
         if (instanceof(currentObj, "IsoPlayer") == true) then
             -- Cows: Check and make sure it is NOT the current player and is alive
@@ -32,12 +34,28 @@ function PZNS_ContextMenuInvite(mpPlayerID, context, worldobjects)
                             npcSurvivor.groupID, npcSurvivor.survivorID
                         );
                     end
+                    npcSurvivor.canSaveData = true;                                  -- Cows: This will allow the NPC to be saved.
                     PZNS_UtilsNPCs.PZNS_SetNPCGroupID(npcSurvivor, playerGroupID);   -- Cows: Update the npcSurvivor groupID
                     PZNS_NPCGroupsManager.addNPCToGroup(npcSurvivor, playerGroupID); -- Cows: Add the npcSurvivor to the group's moddata
+                    PZNS_UtilsDataNPCs.PZNS_SaveNPCData(npcSurvivor.survivorID, npcSurvivor);
                 end
-                -- Cows: Check if the npcSurvivor is NOT in a group or in a different group... Perhaps we can use "affection" at this point for invite-able NPCs.
-                if (npcSurvivor.groupID == nil or npcSurvivor.groupID ~= playerGroupID) then
-                    isInviteeFound = true;
+                -- Cows: Check if the npc is not a raider, raiders cannot be invited
+                if (npcSurvivor.isRaider ~= true) then
+                    --  Cows: Survivor affection must be above a set value to be invited
+                    if (npcSurvivor.affection > 30) then
+                        -- Cows: Ungrouped NPC
+                        if (npcSurvivor.groupID == nil) then
+                            invitableCount = invitableCount + 1;
+                            canInvite = true;
+                        elseif (npcSurvivor.groupID ~= playerGroupID) then
+                            -- Cows: Else different grouped NPCs
+                            invitableCount = invitableCount + 1;
+                            canInvite = true;
+                        end
+                    end
+                end
+                -- Cows: Check if current NPC is invitable.
+                if (canInvite == true) then
                     inviteSubMenu:addOption(
                         npcSurvivor.survivorName,
                         nil,
@@ -47,8 +65,8 @@ function PZNS_ContextMenuInvite(mpPlayerID, context, worldobjects)
             end
         end
     end
-    --
-    if (isInviteeFound == true) then
+    -- Cows: Check if there are more than 0 invitable NPCs.
+    if (invitableCount > 0) then
         local inviteSubMenu_Option = context:addOption(
             getText("ContextMenu_PZNS_PZNS_Invite"),
             worldobjects,
