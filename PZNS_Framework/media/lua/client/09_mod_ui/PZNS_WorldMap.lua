310cdf6e4c71b6d36d786ad8315d7d729ce97386
lanceris <zelchenko.andrey@gmail.com>
2023-09-08 20:31:17 +0200

    Code cleanup

--- PZNS_Framework/media/lua/client/09_mod_ui/PZNS_WorldMap.lua
+++ PZNS_Framework/media/lua/client/09_mod_ui/PZNS_WorldMap.lua
@@ -32,6 +32,12 @@ function ButtonShowLocators:new(x, y, width, height, title, clicktarget, onclick
     return o;
 end
 
+local function updateMembersNpcs(groupID)
+    local members = PZNS_NPCGroupsManager.getMembers(groupID);
+    local activeNPCs = PZNS_UtilsDataNPCs.PZNS_GetCreateActiveNPCsModData();
+    return members, activeNPCs
+end
+
 -- Cows: Encapsulate a few items into a single function; since these items are only relevant to world map rendering.
 function PZNS_UpdateISWorldMapRender()
     local worldmap_render = ISWorldMap.render;
@@ -57,6 +63,10 @@ function PZNS_UpdateISWorldMapRender()
         btnShowGroupMembersLocation:setAlwaysOnTop(true);
     end
     createButtonShowGroupMembers();
+    local ticks = 100
+    local playerGroupID = "Player0Group";
+    local members = PZNS_NPCGroupsManager.getMembers(playerGroupID);
+    local activeNPCs = PZNS_UtilsDataNPCs.PZNS_GetCreateActiveNPCsModData();
     -- Cows: Not sure how to feel about the forced override... but it seems to work without any problems.
     ISWorldMap.render = function(self)
         worldmap_render(self);
@@ -64,57 +74,54 @@ function PZNS_UpdateISWorldMapRender()
         if isShowingGroupMembers ~= true then
             return;
         end
-        local playerGroupID = "Player0Group";
-        local group = PZNS_NPCGroupsManager.getGroupByID(playerGroupID);
-        local activeNPCs = PZNS_UtilsDataNPCs.PZNS_GetCreateActiveNPCsModData();
+
+        -- probably shouldn't update members/npcs every render tick
+        ticks = ticks - 1
+        if ticks <= 0 then
+            ticks = 100
+            members, activeNPCs = updateMembersNpcs(playerGroupID)
+        end
+
         -- Cows: check if activeNPCs is not nil and loaded.
-        if (activeNPCs == nil or group == nil) then
+        if (activeNPCs == nil or members == {}) then
             return;
         end
         -- Cows: iterate through the survivorIDs of the group.
-        for survivorID, v in pairs(group) do
+        for i = 1, #members do
+            local survivorID = members[i]
             local npcSurvivor = activeNPCs[survivorID];
             --
             if (npcSurvivor ~= nil) then
                 local npcIsoPlayer = npcSurvivor.npcIsoPlayerObject;
                 local survivorName = npcSurvivor.survivorName;
+                local survivorX
+                local survivorY
+                if npcIsoPlayer and npcIsoPlayer:isAlive() then
+                    survivorX = npcIsoPlayer:getX()
+                    survivorY = npcIsoPlayer:getY()
+                elseif npcSurvivor.isAlive then
+                    survivorX = npcSurvivor.squareX
+                    survivorY = npcSurvivor.squareY
+                end
                 --
-                if (npcIsoPlayer ~= nil) then
-                    if (npcIsoPlayer:isAlive() == true) then
-                        local x = self.mapAPI:worldToUIX(npcIsoPlayer:getX(), npcIsoPlayer:getY()) - 3;
-                        local y = self.mapAPI:worldToUIY(npcIsoPlayer:getX(), npcIsoPlayer:getY()) - 3;
-                        self:drawRect(x, y, 6, 6, 1, 0, 0, 1);     -- Cows: This draws the square dot on the map.
-                        local name_size = getTextManager():MeasureStringX(UIFont.NewSmall, survivorName);
-                        self:drawRect(
-                            x - 6,             -- y offset, should put the box above the dot and the text in middle.
-                            y - 28,            -- y offset, should put the box above the dot and the text in center.
-                            name_size + 3,     -- Width
-                            24,                -- Height
-                            0.5,               -- Transparency
-                            0,                 -- R
-                            0,                 -- G
-                            0                  -- B
-                        );                     -- Cows: This draws the namebox background
-                        self:drawText(survivorName, x - 5, y - (28 + 1), 1, 1, 1, 1, UIFont.NewSmall);
-                    end
-                elseif (npcSurvivor.isAlive == true) then
-                    local x = self.mapAPI:worldToUIX(npcSurvivor.squareX, npcSurvivor.squareY) - 3;
-                    local y = self.mapAPI:worldToUIY(npcSurvivor.squareX, npcSurvivor.squareY) - 3;
-                    self:drawRect(x, y, 6, 6, 1, 0, 0, 1);     -- Cows: This draws the square dot on the map.
+                if survivorX and survivorY then
+                    local x = self.mapAPI:worldToUIX(survivorX, survivorY) - 3;
+                    local y = self.mapAPI:worldToUIY(survivorX, survivorY) - 3;
+                    self:drawRect(x, y, 6, 6, 1, 0, 0, 1); -- Cows: This draws the square dot on the map.
                     local name_size = getTextManager():MeasureStringX(UIFont.NewSmall, survivorName);
                     self:drawRect(
-                        x - 6,             -- y offset, should put the box above the dot and the text in middle.
-                        y - 28,            -- y offset, should put the box above the dot and the text in center.
-                        name_size + 3,     -- Width
-                        24,                -- Height
-                        0.5,               -- Transparency
-                        0,                 -- R
-                        0,                 -- G
-                        0                  -- B
-                    );                     -- Cows: This draws the namebox background
+                        x - 6,         -- y offset, should put the box above the dot and the text in middle.
+                        y - 28,        -- y offset, should put the box above the dot and the text in center.
+                        name_size + 3, -- Width
+                        24,            -- Height
+                        0.5,           -- Transparency
+                        0,             -- R
+                        0,             -- G
+                        0              -- B
+                    );                 -- Cows: This draws the namebox background
                     self:drawText(survivorName, x - 5, y - (28 + 1), 1, 1, 1, 1, UIFont.NewSmall);
                 end
             end
-        end     -- Cows: End group for-loop
+        end -- Cows: End group for-loop
     end
 end
